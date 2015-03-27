14324f42aeb06c9ff93a005c7b09388f6b9169bd
Calogero Ramirez <frcalo5@vt.edu>
2015-03-26 21:13:29 -0400

    Make Coffee Class

---
+++ src/cs2114/restaurantmanager/Coffee.java
@@ -0,0 +1,21 @@
+package cs2114.restaurantmanager;
+/**
+ *  Coffee class that contains the menu item's
+ *  specifications.
+ *
+ *  @author Calogero Ramirez (frcalo5)
+ *  @version Mar 26, 2015
+ */
+public class Coffee extends FoodItem
+{
+    /**
+     * Create a new Coffee object.
+     */
+    public Coffee()
+    {
+        name = "Coffee";
+        ingredients = "Served with choice of expresso shots, "
+            + "flavored syrups or whipped cream.";
+        cost = 1.49;
+    }
+}
