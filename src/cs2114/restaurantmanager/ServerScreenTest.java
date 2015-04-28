3be6bbaf6f6248a3c66ec219376bdd0d46355526
Calogero Ramirez <frcalo5@vt.edu>
2015-04-27 20:11:25 -0400

    Tests for server screen

---
+++ src/cs2114/restaurantmanager/ServerScreenTest.java
@@ -0,0 +1,218 @@
+package cs2114.restaurantmanager;
+
+import android.content.Intent;
+import android.widget.Button;
+import junit.framework.TestCase;
+
+/**
+ *  A suite of test cases to test the behavior of the
+ *  ServerScreen class.
+ *
+ *  @author Calogero Ramirez (frcalo5)
+ *  @version 2015.04.27
+ */
+public class ServerScreenTest
+    extends student.AndroidTestCase<ServerScreen>
+{
+    /**
+     * A field for the serverToMenu Button
+     */
+    private Button serverToMenu;
+    private Restaurant restaurant;
+    private Button table1;
+    private Button table2;
+    private Button table3;
+    private Button table4;
+    private Button table5;
+    private Button table6;
+    private Button table7;
+    private Button table8;
+    private Button table9;
+    private Button table10;
+    private Button table11;
+    private Button table12;
+    private Button table13;
+    private Button table14;
+    private Button table15;
+    private Button table16;
+    private Button table17;
+    private Button table18;
+    private Button table19;
+    private Button table20;
+    /**
+     * Create a new ServerScreenTest object.
+     */
+    public ServerScreenTest()
+    {
+        super(ServerScreen.class);
+    }
+    /**
+     * Test to check the functionality of serverToMenuClicked()
+     */
+    public void testServerToMenu()
+    {
+        prepareForUpcomingActivity(Intent.ACTION_VIEW);
+        click(serverToMenu);
+    }
+    /**
+     * Tests the function of the table1 button
+     */
+    public void testTable1()
+    {
+        prepareForUpcomingActivity(Intent.ACTION_VIEW);
+        click(table1);
+    }
+    /**
+     * Tests the function of the table2 button
+     */
+    public void testTable2()
+    {
+        prepareForUpcomingActivity(Intent.ACTION_VIEW);
+        click(table2);
+    }
+    /**
+     * Tests the function of the table3 button
+     */
+    public void testTable3()
+    {
+        prepareForUpcomingActivity(Intent.ACTION_VIEW);
+        click(table3);
+    }
+    /**
+     * Tests the function of the table4 button
+     */
+    public void testTable4()
+    {
+        prepareForUpcomingActivity(Intent.ACTION_VIEW);
+        click(table4);
+    }
+    /**
+     * Tests the function of the table5 button
+     */
+    public void testTable5()
+    {
+        prepareForUpcomingActivity(Intent.ACTION_VIEW);
+        click(table5);
+    }
+    /**
+     * Tests the function of the table6 button
+     */
+    public void testTable6()
+    {
+        prepareForUpcomingActivity(Intent.ACTION_VIEW);
+        click(table6);
+    }
+    /**
+     * Tests the function of the table7 button
+     */
+    public void testTable7()
+    {
+        prepareForUpcomingActivity(Intent.ACTION_VIEW);
+        click(table7);
+    }
+    /**
+     * Tests the function of the table8 button
+     */
+    public void testTable8()
+    {
+        prepareForUpcomingActivity(Intent.ACTION_VIEW);
+        click(table8);
+    }
+    /**
+     * Tests the function of the table9 button
+     */
+    public void testTable9()
+    {
+        prepareForUpcomingActivity(Intent.ACTION_VIEW);
+        click(table9);
+    }
+    /**
+     * Tests the function of the table10 button
+     */
+    public void testTable10()
+    {
+        prepareForUpcomingActivity(Intent.ACTION_VIEW);
+        click(table10);
+    }
+    /**
+     * Tests the function of the table11 button
+     */
+    public void testTable11()
+    {
+        prepareForUpcomingActivity(Intent.ACTION_VIEW);
+        click(table11);
+    }
+    /**
+     * Tests the function of the table12 button
+     */
+    public void testTable12()
+    {
+        prepareForUpcomingActivity(Intent.ACTION_VIEW);
+        click(table12);
+    }
+    /**
+     * Tests the function of the table13 button
+     */
+    public void testTable13()
+    {
+        prepareForUpcomingActivity(Intent.ACTION_VIEW);
+        click(table13);
+    }
+    /**
+     * Tests the function of the table14 button
+     */
+    public void testTable14()
+    {
+        prepareForUpcomingActivity(Intent.ACTION_VIEW);
+        click(table14);
+    }
+    /**
+     * Tests the function of the table15 button
+     */
+    public void testTable15()
+    {
+        prepareForUpcomingActivity(Intent.ACTION_VIEW);
+        click(table15);
+    }
+    /**
+     * Tests the function of the table16 button
+     */
+    public void testTable16()
+    {
+        prepareForUpcomingActivity(Intent.ACTION_VIEW);
+        click(table16);
+    }
+    /**
+     * Tests the function of the table17 button
+     */
+    public void testTable17()
+    {
+        prepareForUpcomingActivity(Intent.ACTION_VIEW);
+        click(table17);
+    }
+    /**
+     * Tests the function of the table18 button
+     */
+    public void testTable18()
+    {
+        prepareForUpcomingActivity(Intent.ACTION_VIEW);
+        click(table18);
+    }
+    /**
+     * Tests the function of the table19 button
+     */
+    public void testTable19()
+    {
+        prepareForUpcomingActivity(Intent.ACTION_VIEW);
+        click(table19);
+    }
+    /**
+     * Tests the function of the table20 button
+     */
+    public void testTable20()
+    {
+        prepareForUpcomingActivity(Intent.ACTION_VIEW);
+        click(table20);
+    }
+
+}
